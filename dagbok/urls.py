470939b2248fe9173f10d3fb45df9b791f6306ec
Kristian Robertsen <kristian.robertsen@gmail.com>
2018-02-12 23:43:50 +0100

    Legge til fullført prosjektbeskrivelse; begynne å klargjøre for riktig deployment

--- dagbok/urls.py
+++ dagbok/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import (Landing, Dagbok, PostCreate, PostEdit, PostDelete)
 from organizer.views import Medlem
@@ -31,4 +33,4 @@ urlpatterns = [
         name="dagbok_post_delete"
     ),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
