64923389d60750df8e280176553f4376a86e3c9c
Kristian Robertsen <kristian.robertsen@gmail.com>
2018-04-22 18:42:58 +0200

    Legg til statusrapport for MP2, linker til øvrige dokumenter

--- organizer/urls.py
+++ organizer/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import (Home, ProsjektSkisse, ProsjektBeskrivelse)
+from .views import (Home, ProsjektSkisse, ProsjektBeskrivelse, ProsjektDocs, ProsjektStatus)
 
 urlpatterns = [
     url(r'^$', Home.as_view(), name='home'),
+    url(r'^docs/$', ProsjektDocs.as_view(), name='docs'),
     url(r'^skisse/$', ProsjektSkisse.as_view(), name='skisse'),
+    url(r'^status/$',ProsjektStatus.as_view(), name='status'),
     url(r'^beskrivelse/$', ProsjektBeskrivelse.as_view(), name='beskrivelse'),
 ]
--- organizer/views.py
+++ organizer/views.py
@@ -19,3 +19,13 @@ class ProsjektBeskrivelse(View):
     template_name = 'organizer/beskrivelse.html'
     def get(self, request):
         return render(request, self.template_name, {})
+
+class ProsjektDocs(View):
+    template_name = 'organizer/docs.html'
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+class ProsjektStatus(View):
+    template_name = 'organizer/status.html'
+    def get(self, request):
+        return render(request, self.template_name, {})
